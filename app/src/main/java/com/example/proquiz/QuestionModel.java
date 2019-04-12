fc51c53fa7b5645c8dfaeabd2dd9ee645cb8060f
Anwar <anwarrabiu81@gmail.com>
2019-04-13 02:40:32 +0300

    This is Anwar's quiz app

---
+++ app/src/main/java/com/example/proquiz/QuestionModel.java
@@ -0,0 +1,22 @@
+package com.example.proquiz;
+
+public class QuestionModel {
+    public QuestionModel(String questionString, String answer){
+        QuestionString = questionString;
+        Answer = answer;
+    }
+    public String getQuestionString(){
+        return QuestionString;
+    }
+    public void setQuestionString(String questionString){
+        QuestionString = questionString;
+    }
+    public String getAnswer(){
+        return Answer;
+    }
+    public void setAnswer(String answer){
+        Answer = answer;
+    }
+    private String QuestionString;
+    private String Answer;
+}
